8d5c28f5b893ab5034a9386f799280447a558505
Coleman Nitroy <cnitroy@me.com>
2017-02-15 15:56:23 -0800

    Adding education and design_team channels

---
+++ messages/design_team.js
@@ -0,0 +1,20 @@
+module.exports = {
+  link_names: true, 
+  as_user: true, 
+  unfurl_links: true,
+  attachments: [
+    {
+      "title": "Welcome to the #design_team channel!",
+      "text": "Print, digial all of the above. The #design_team channel is for making that happen.",
+      "mrkdwn_in": ["text"],
+      "color": "#ff0000",
+      "footer": "Seattle DSA Slackbot",
+      "footer_icon": "https://pbs.twimg.com/profile_images/809817207358095360/SZTlT4d6_400x400.jpg"      
+    },
+    {
+      "title": "SDSA_Design_Team_Onboarding.zip",
+      "title_link": "https://seattledsa.org/wp-content/uploads/2017/02/SDSA_Design_Team_Onboarding.zip",
+      "text": "Start here."
+    }
+  ]
+};
---
+++ messages/education.js
@@ -0,0 +1,25 @@
+module.exports = {
+  link_names: true, 
+  as_user: true, 
+  unfurl_links: true,
+  attachments: [
+    {
+      "title": "Welcome to the #education channel!",
+      "text": "Talk organize reading groups, presentations, and any other ways of increasing our collective knowledge of the world.",
+      "mrkdwn_in": ["text"],
+      "color": "#ff0000",
+      "footer": "Seattle DSA Slackbot",
+      "footer_icon": "https://pbs.twimg.com/profile_images/809817207358095360/SZTlT4d6_400x400.jpg"      
+    },
+    {
+      "title": "Education Google Drive",
+      "title_link": "https://drive.google.com/drive/folders/0BzRdPX7Rqp6Xei1ON1hFVnBtT00?ths=true",
+      "text": "Readings, presentations, and all other useful documents."
+    },
+    {
+      "title": "ABCs of socialism lesson plan",
+      "title_link": "https://docs.google.com/document/d/1MU2XkD8CZc-szcUDhFGEFouJQyY_sxhfIHDPbJLQFVg/edit",
+      "text": "For our first reading group (March 18th). All input is welcome."
+    }
+  ]
+};
