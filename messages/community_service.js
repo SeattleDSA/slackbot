e30df6cea35b9eaced0dc98ca8eee06e880cb425
Coleman Nitroy <cnitroy@me.com>
2017-08-26 11:19:41 -0700

    Adding community service channel welcome message

---
+++ messages/community_service.js
@@ -0,0 +1,20 @@
+module.exports = {
+  link_names: true, 
+  as_user: true, 
+  unfurl_links: true,
+  attachments: [
+    {
+      "title": "Welcome to the #community_service DSA channel!",
+      "text": "Welcome to the Community Service DSA channel! Here we talk about community outreach, service projects, donations, and drives. We also meet once a month to discuss ongoing community service work. Want to get involved? Reach out to ",
+      "mrkdwn_in": ["text"],
+      "color": "#ff0000",
+      "footer": "Seattle DSA Slackbot",
+      "footer_icon": "https://pbs.twimg.com/profile_images/809817207358095360/SZTlT4d6_400x400.jpg"      
+    },
+    {
+      "title": "Want to get involved?",
+      "title_link": "communityservice@seattledsa.org",
+      "text": "Reach out via our email"
+    }    
+  ]
+};
