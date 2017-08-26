80d20a8206350511150487b71bb62d4d7c3bda8f
Coleman Nitroy <cnitroy@me.com>
2017-08-26 11:29:43 -0700

    Fixing community_service email address

--- messages/community_service.js
+++ messages/community_service.js
@@ -5,16 +5,11 @@ module.exports = {
   attachments: [
     {
       "title": "Welcome to the #community_service DSA channel!",
-      "text": "Welcome to the Community Service DSA channel! Here we talk about community outreach, service projects, donations, and drives. We also meet once a month to discuss ongoing community service work. Want to get involved? Reach out to ",
+      "text": "Welcome to the Community Service DSA channel! Here we talk about community outreach, service projects, donations, and drives. We also meet once a month to discuss ongoing community service work. Want to get involved? Reach out to communityservice@seattledsa.org",
       "mrkdwn_in": ["text"],
       "color": "#ff0000",
       "footer": "Seattle DSA Slackbot",
       "footer_icon": "https://pbs.twimg.com/profile_images/809817207358095360/SZTlT4d6_400x400.jpg"      
-    },
-    {
-      "title": "Want to get involved?",
-      "title_link": "communityservice@seattledsa.org",
-      "text": "Reach out via our email"
-    }    
+    }
   ]
 };
